c74f914e59511ff52ec1e12c2ee222fa1e782e2a
qhristen <odeyxtian@gmail.com>
2025-01-30 19:14:25 +0100

    Update package.json dependencies and refactor escrow program functions for clarity

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -12,7 +12,7 @@ declare_id!("JAHbbouZUU7BipZ43LfLPBVnMygfhxhkA1PJWVz8HWMh");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize_escrow(
+    pub fn make(
         ctx: Context<Make>,
         seed: u64,
         recieve_amount: u64,
@@ -28,7 +28,7 @@ pub mod escrow {
         Ok(())
     }
 
-    pub fn taker_deposit_withdraw_and_close(ctx: Context<Take>) -> Result<()> {
+    pub fn take(ctx: Context<Take>) -> Result<()> {
         //The Taker of an escrow can deposit tokens of mint_b to the maker and recieve tokens of mint_a that the maker deposited.
 
         msg!("Deposit amount requested by the maker");
@@ -40,7 +40,7 @@ pub mod escrow {
         Ok(())
     }
 
-    pub fn make_refunc(ctx: Context<Refund>) -> Result<()> {
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
         //The maker of an escrow can refund the tokens from the vault and close the escrow account.
         msg!("Deposit amount requested by the maker");
         ctx.accounts.refund_and_close_vault()?;
